1cfda08a7df9b31698524af5d7d96640b0d09d5b
geertj <geertj@localhost>
2010-04-02 18:46:35 +0200

    python-rest-1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name = 'python-rest',
-    version = '1.0',
+    version = '1.1',
     description = 'A mini-framework for creating RESTful applications.',
     author = 'Geert Jansen',
     author_email = 'geertj@gmail.com',
