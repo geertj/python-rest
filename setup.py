5972a149cb3e11a57df4275fb2b572b53f371995
geertj <geertj@localhost>
2010-04-28 16:27:58 +0200

    Include automatic version from setup.py.

--- setup.py
+++ setup.py
@@ -6,24 +6,66 @@
 # Python-REST is copyright (c) 2010 by the Python-REST authors. See the file
 # "AUTHORS" for a complete overview.
 
+import sys
+import os.path
+import inspect
+
+from distutils.command.build import build
 from setuptools import setup
 
-setup(
-    name = 'python-rest',
-    version = '1.1',
-    description = 'A mini-framework for creating RESTful applications.',
-    author = 'Geert Jansen',
-    author_email = 'geertj@gmail.com',
-    url = 'http://bitbucket.org/geertj/python-rest',
-    license = 'MIT',
-    classifiers = [
+
+version_info = {
+    'name': 'python-rest',
+    'version': '1.1',
+    'description': 'A mini-framework for creating RESTful applications.',
+    'author': 'Geert Jansen',
+    'author_email': 'geertj@gmail.com',
+    'url': 'http://bitbucket.org/geertj/python-rest',
+    'license': 'MIT',
+    'classifiers': [
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python'],
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application' ]
+}
+
+
+class mybuild(build):
+
+    def _topdir(self):
+        fname = inspect.getfile(sys.modules['__main__'])
+        absname = os.path.abspath(fname)
+        return os.path.split(absname)[0]
+
+    def _store_version(self):
+        fname = os.path.join(self._topdir(), 'lib', 'rest', '_version.py')
+        contents = '# This is a geneated file - do not edit!\n'
+        version = tuple(map(int, version_info['version'].split('.')))
+        contents += 'version = %s\n' % repr(version)
+        try:
+            fin = file(fname, 'r')
+        except IOError:
+            current = None
+        else:
+            current = fin.read()
+            fin.close()
+        if contents != current:
+            fout = file(fname, 'w')
+            fout.write(contents)
+            fout.close()
+
+    def run(self):
+        self._store_version()
+        build.run(self)
+
+
+setup(
     package_dir = {'': 'lib'},
     packages = ['rest', 'rest.test'],
-    test_suite = 'nose.collector'
+    test_suite = 'nose.collector',
+    cmdclass = { 'build': mybuild },
+    **version_info
 )
