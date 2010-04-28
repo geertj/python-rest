4b78944db19e6d510211d56a0da25febba34a875
Geert Jansen <geertj@gmail.com>
2010-04-28 16:30:02 +0200

    Version 1.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 version_info = {
     'name': 'python-rest',
-    'version': '1.1',
+    'version': '1.2',
     'description': 'A mini-framework for creating RESTful applications.',
     'author': 'Geert Jansen',
     'author_email': 'geertj@gmail.com',
