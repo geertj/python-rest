8f53459737486b2243eb01751423829bfaa6c816
Geert Jansen <geertj@gmail.com>
2010-06-08 22:09:04 +0200

    For now disable outputting of tags in YAML output, until there's some clarity whether unknown tags are supported by the majority of YAML implementations out there.

--- lib/rest/entity.py
+++ lib/rest/entity.py
@@ -207,9 +207,12 @@ class FormatEntity(OutputFilter):
         def format(self, data):
             if '!type' in data:
                 data = data.copy()
-                tag = '!%s' % data.pop('!type')
-            else:
-                tag = u'tag:yaml.org,2002:map'
+                data.pop('!type')
+            # XXX: for now do not output tags in YAML but if possible i'd like
+            # to re-instate this if it appears that YAML parsers can handle
+            # unknown tags reasonably well (PyYAML doesnt'...)
+            #tag = '!%s' % data.pop('!type')
+            tag = u'tag:yaml.org,2002:map'
             return loader.represent_mapping(tag, data)
         return format
 
