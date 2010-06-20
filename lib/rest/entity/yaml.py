a29808241b2c2fe801b0eef226374f9d2a9d955c
Geert Jansen <geertj@gmail.com>
2010-06-20 19:14:00 -0400

    Factor out PyYAML setup so that it can be reused by clients.

--- lib/rest/entity/yaml.py
+++ lib/rest/entity/yaml.py
@@ -10,67 +10,43 @@ from __future__ import absolute_import
 
 import yaml
 from yaml import YAMLError
-from StringIO import StringIO
 
+from rest import http
+from rest.util import _pyyaml_construct_resources, _pyyaml_represent_resources
 from rest.error import HTTPReturn
 from rest.resource import Resource
 from rest.entity.parse import Parser
 from rest.entity.format import Formatter
 
+_pyyaml_construct_resources()
+_pyyaml_represent_resources()
+
 
 class YAMLParser(Parser):
     """Parse an entity in YAML format to native representation."""
-
-    def _construct(self, loader):
-        """Construct a resource node."""
-        def construct(self, node):
-            if isinstance(node, yaml.MappingNode):
-                mapping = loader.construct_mapping(node)
-                resource = Resource(node.tag[1:], mapping)
-            return resource
-        return construct
 
     def parse(self, input, encoding=None):
         """Parse a YAML entity."""
         # We can ignore the encoding as the YAML spec mandates either UTF-8
         # or UTF-16 with a BOM, which can be autodetected.
         # We use a Loader that turns unrecognized !tags into Resources.
-        loader = yaml.Loader(input)
-        loader.add_constructor(None, self._construct(loader))
         try:
-            parsed = loader.get_single_data()
+            parsed = yaml.load(input)
         except YAMLError, e:
             raise HTTPReturn(http.BAD_REQUEST,
-                             reason='YAML Error: %s' % str(e))
+                             reason='YAML load error: %s' % str(e))
         return parsed
 
 
 class YAMLFormatter(Formatter):
     """Format an entity in native representation to YAML."""
 
-    def _represent_resource(self, loader):
-        """Used as a YAML representer for dictionaries."""
-        def format(self, data):
-            if '!type' in data:
-                data = data.copy()
-                tag = '!%s' % data.pop('!type')
-            else:
-                tag = u'tag:yaml.org,2002:map'
-            return loader.represent_mapping(tag, data)
-        return format
-
     def format(self, object, encoding=None):
         """Format a resource as YAML under the specified encoding."""
-        stream = StringIO()
-        dumper = yaml.Dumper(stream, default_flow_style=False, version=(1,1),
-                             encoding=encoding)
-        dumper.add_representer(dict, self._represent_resource(dumper))
-        dumper.add_representer(Resource, self._represent_resource(dumper))
-        dumper.open()
         try:
-            dumper.represent(object)
+            output = yaml.dump(object, default_flow_style=False,
+                               version=(1, 1), encoding=encoding)
         except YAMLError, e:
             raise HTTPReturn(http.INTERNAL_SERVER_ERROR,
                              reason='YAML dump error: %s' % str(e))
-        dumper.close()
-        return stream.getvalue()
+        return output
